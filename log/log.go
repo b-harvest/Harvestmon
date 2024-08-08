60356343fa9ed437d9d17e0cdca9956daaafaff9
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2024-08-08 13:53:37 +0900

    feat: Add log debugging option, changing color for warn to yellow, and also changing timeformat

--- log/log.go
+++ log/log.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"runtime/debug"
@@ -16,9 +17,19 @@ var eventQueue chan func()
 func init() {
 	// Logger setup
 	output := zerolog.ConsoleWriter{
-		Out:        os.Stderr,
-		TimeFormat: time.RFC1123,
+		Out:         os.Stderr,
+		TimeFormat:  time.RFC3339,
+		FormatLevel: logColorFormatter(),
 	}
+
+	logLevelDebug := flag.Bool("debug", false, "allow showing debug log")
+	flag.Parse()
+	if *logLevelDebug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+
 	log.Logger = log.Output(output)
 
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
@@ -74,4 +85,50 @@ func Debug(msg any) {
 		log.Debug().Msg(message)
 	}
 	enqueue(event)
+}
+
+const (
+	colorBlack = iota + 30
+	colorRed
+	colorGreen
+	colorYellow
+	colorBlue
+	colorMagenta
+	colorCyan
+	colorWhite
+
+	colorBold     = 1
+	colorDarkGray = 90
+)
+
+func logColorFormatter() func(interface{}) string {
+	return func(i interface{}) string {
+		var l string
+		if ll, ok := i.(string); ok {
+			switch ll {
+			case zerolog.LevelTraceValue:
+				l = colorize("TRC", colorMagenta)
+			case zerolog.LevelDebugValue:
+				l = colorize("DBG", colorYellow)
+			case zerolog.LevelInfoValue:
+				l = colorize("INF", colorGreen)
+			case zerolog.LevelWarnValue:
+				l = colorize("WRN", colorYellow)
+			case zerolog.LevelErrorValue:
+				l = colorize(colorize("ERR", colorRed), colorBold)
+			case zerolog.LevelFatalValue:
+				l = colorize(colorize("FTL", colorRed), colorBold)
+			case zerolog.LevelPanicValue:
+				l = colorize(colorize("PNC", colorRed), colorBold)
+			default:
+				l = colorize(ll, colorBold)
+			}
+		}
+		return l
+	}
+}
+
+// colorize returns the string s wrapped in ANSI code c, unless disabled is true.
+func colorize(s interface{}, c int) string {
+	return fmt.Sprintf("\x1b[%dm%v\x1b[0m", c, s)
 }
