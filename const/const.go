a73191836a654a6979f1b6d3838384130a77c9a1
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2024-08-19 11:08:01 +0900

    feat: Add const

--- const/const.go
+++ const/const.go
@@ -1,3 +1,9 @@
 package _const
 
-const HARVESTMON_TENDERMINT_SERVICE_NAME = "tendermint"
+const (
+	HARVESTMON_TENDERMINT_SERVICE_NAME = "tendermint"
+	TM_EVENT_TYPE                      = "tm:event"
+	TM_STATUS_EVENT_TYPE               = TM_EVENT_TYPE + ":status"
+	TM_NET_INFO_EVENT_TYPE             = TM_EVENT_TYPE + ":net_info"
+	TM_COMMIT_EVENT_TYPE               = TM_EVENT_TYPE + ":commit"
+)
