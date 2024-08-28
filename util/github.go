0d1cacfa9655f6011a8595db0c9b93054c2c1a33
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2024-08-28 15:30:36 +0900

    feat: Update githubUtil

--- util/github.go
+++ util/github.go
@@ -13,7 +13,11 @@ type GitHubFile struct {
 	Content string `json:"content"`
 }
 
-func FetchGithubFile(owner, repo, branch, path, token string) ([]byte, error) {
+type GitHubPath struct {
+	Path string `json:"path"`
+}
+
+func FetchGithubFile(owner, repo, branch, path, token string) ([]string, error) {
 	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/contents/%s?ref=%s", owner, repo, path, branch)
 
 	req, _ := http.NewRequest("GET", url, nil)
@@ -27,14 +31,36 @@ func FetchGithubFile(owner, repo, branch, path, token string) ([]byte, error) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New(fmt.Sprintf("Failed to fetch file:", resp.Status))
+		return nil, errors.New(fmt.Sprintf("Failed to fetch file: %s", resp.Status))
 	}
+
+	var result []string
 
 	body, _ := ioutil.ReadAll(resp.Body)
 
 	var file GitHubFile
 	err = json.Unmarshal(body, &file)
+	if err != nil {
+		var githubPaths []GitHubPath
+		err = json.Unmarshal(body, &githubPaths)
+		if err != nil {
+			return nil, err
+		}
+		for _, githubPath := range githubPaths {
+			content, err := FetchGithubFile(owner, repo, branch, githubPath.Path, token)
+			if err != nil {
+				return nil, err
+			}
 
-	decodedContent, err := base64.StdEncoding.DecodeString(file.Content)
-	return decodedContent, err
+			result = append(result, content...)
+		}
+	} else {
+		decodedContent, err := base64.StdEncoding.DecodeString(file.Content)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, string(decodedContent))
+	}
+
+	return result, err
 }
