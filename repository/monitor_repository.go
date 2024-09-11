07ca3d9e90e8bb4436ac6ab20fa64c35cc91500e
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2024-09-11 10:47:33 +0900

    feat: Add meta monitor repository

---
+++ repository/monitor_repository.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"errors"
+	"fmt"
+)
+
+type MetaMonitor struct {
+	AgentName string `gorm:"primaryKey;column:agent_name;not null;type:varchar(50)"`
+	Height    string `gorm:"column:height;not null;type:bigint"`
+}
+
+func (MetaMonitor) TableName() string {
+	return "meta_monitor"
+}
+
+type MetaMonitorRepository struct {
+	BaseRepository
+}
+
+func (r *MetaMonitorRepository) Save(metaMonitor MetaMonitor) error {
+	res := r.DB.Save(&metaMonitor)
+	if res.Error != nil {
+		return res.Error
+	}
+
+	return nil
+}
+
+func (r *MetaMonitorRepository) FetchHighestHeight(agentName string) (uint64, error) {
+	var (
+		maxHeight uint64
+	)
+	err := r.DB.Raw(`select /*+ USE INDEX (INDEX_AGENT_NAME_HEIGHT) */ max(height)
+from meta_monitor
+where agent_name = ?
+order by height desc
+limit 1;`, agentName).Scan(&maxHeight).Error
+
+	if err != nil {
+		return 0, errors.New(fmt.Sprintf("failed to get maximum height: %v", err))
+	}
+
+	return maxHeight, nil
+}
