9361700f1cfe16a5aa44a750fea22a5f05bcc9c8
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2024-12-02 15:48:09 +0900

    chore: Add ethereum harvestmon version

--- const/const.go
+++ const/const.go
@@ -6,4 +6,9 @@ const (
 	TM_STATUS_EVENT_TYPE               = TM_EVENT_TYPE + ":status"
 	TM_NET_INFO_EVENT_TYPE             = TM_EVENT_TYPE + ":net_info"
 	TM_COMMIT_EVENT_TYPE               = TM_EVENT_TYPE + ":commit"
+
+	HARVESTMON_ETHEREUM_SERVICE_NAME = "ethereum"
+	ETH_EVENT_TYPE                   = "eth:event"
+	ETH_BLOCK_NUMBER_EVENT_TYPE      = ETH_EVENT_TYPE + ":block_number"
+	ETH_IS_SYNCING_EVENT_TYPE        = ETH_EVENT_TYPE + ":is_syncing"
 )
