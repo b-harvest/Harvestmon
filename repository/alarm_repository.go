f597778738f1c18ddeaafa357e66cf55ea299e7b
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2025-01-20 19:28:09 +0900

    update

--- repository/alarm_repository.go
+++ repository/alarm_repository.go
@@ -64,7 +64,7 @@ UPDATE active_alarm
 SET alert_record_sent_time = ?
 WHERE alarmer_name = ?
 AND instance = ?
-AND strategy_target = ?
+AND target = ?
 `, ts, alarm.AlarmerName, alarm.Instance, alarm.Target).Error
 	if err != nil {
 		return err
@@ -80,7 +80,7 @@ func (r *Repository) DeleteActiveAlarms(alist []ActiveAlarm) error {
 
 	// Generate the conditions for a raw SQL query
 	var params []interface{}
-	query := "DELETE FROM active_alarm WHERE (instance, strategy_target, alarmer_name) IN ("
+	query := "DELETE FROM active_alarm WHERE (instance, target, alarmer_name) IN ("
 
 	for i, alarm := range alist {
 		if i > 0 {
